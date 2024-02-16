8f8d780c6b43ab824b99fcf2915ffb67944ee412
Alter-up <135228265+Alter-up@users.noreply.github.com>
2024-02-16 15:08:36 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
-# ----- 클라이언트 세팅 ----- #
-BOT_TOKEN = "MTIwNzkzNDYxNDA0NDI4NzA0Ng.GXXukd.gb6vR6PG0DJnD_Mk3MwonXlLUxhntmFDHlukO0" # 봇 토큰
-CLIENT_ID = "1207934614044287046" # 클라이언트 아이디
-CLIENT_SECRET = "Ab_av_ohyyErTfDwJf7lKhVTrPukTa7v" # 클라이언트 시크릿
-REDIRECT_URI = "https://discord.com" # 리다이렉트될 주소
-
-# ----- 서버 설정 ----- #
-SERVER_ID = "1207934845108363284" # 가입될 서버 아이디
-
-# ----- 플라스크 실행 설정 ----- #
+BOT_TOKEN = "MTIwNzMwNjYxMTQ3NjAwNDg5NQ.Gtngdu.rbUsnlL0APGYZGSjw3j3tkLiuL0Qp5YJeuWBCo" # 봇 토큰
+CLIENT_ID = "1207306611476004895" # 클라이언트 아이디
+CLIENT_SECRET = "KB4cSRltFrVJmSI7Fp1ljG71KtFo9IfH" # 클라이언트 시크릿
+REDIRECT_URI = "https://google.com" # 리다이렉트될 주소
+SERVER_ID = "1207933093491515422" # 가입될 서버 아이디
 WEB_PORT = "80" # 포트 기본 80포트 (개인사정에 따라 바꾸세요)
 WEB_HOST = "0.0.0.0" # 외부접속 가능한 0.0.0.0 (개인사정에 따라 바꾸세요) 
